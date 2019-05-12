976b0f930f77a41442a52a6dc01e974121ff412a
dholley1 <dholley@hamilton.edu>
2019-05-12 16:29:18 -0400

    set up neural network and node classes

--- project4.py
+++ project4.py
@@ -79,10 +79,61 @@ def accuracy(nn, pairs):
 
 ################################################################################
 ### Neural Network code goes here
+class Node:
+    """ class for each node in neural network """
+    def __init__(self):
+        #weights for each link node
+        self.weights = []
+        #each link node
+        self.links = []
+        #reference to parent nodes for activation function
+        self.parents = []
+        #activation value for self
+        self.activation_value = 1
 
-class NeuralNetwork():
-    def __init__(self, size):
-        pass
+    def __str__(self):
+        """ string representation of node """
+        return "Weigts:\n"+\
+        str(self.weights)+\
+        "\nLinks:\n"+\
+        str(self.links)+\
+        "\nParents:\n"+\
+        str(self.parents)
+
+    def activation_function(self, index):
+        """ activation function to get the weighted value of node """
+        if len(self.parents) > 0:
+            total = 0
+            for parent in self.parents:
+                #recursively call parent activation functions
+                total += parent.activation_function(index)
+            return total * self.activation_value
+        return logistic(self.activation_value)
+
+
+class NeuralNetwork:
+    """ class for neural network """
+    def __init__(self, nodes):
+        """ sets up orientation of nodes in network """
+        self.network = []
+        for i in range(len(nodes)):
+            #setting up network size
+            self.network.append([])
+            for j in range(nodes[i]):
+                #adding nodes
+                newNode = Node()
+                if i < len(nodes)-1:
+                    #giving dummy weights to nodes
+                    newNode.weights = [1]*nodes[i+1]
+                self.network[i].append(newNode)
+            if i > 0:
+                for node in self.network[i-1]:
+                    #setting up links
+                    node.links = self.network[i]
+                    for link in self.network[i]:
+                        #set up reference to parents
+                        link.parents.append(node)
+
 
     def predict_class(self):
         pass
@@ -101,17 +152,20 @@ def main():
     header, data = read_data(sys.argv[1], ",")
 
     pairs = convert_data_to_pairs(data, header)
+    print("pairs:")
+    print(pairs)
 
     # Note: add 1.0 to the front of each x vector to account for the dummy input
     training = [([1.0] + x, y) for (x, y) in pairs]
 
     # Check out the data:
+    print("training:")
     for example in training:
         print(example)
 
     ### I expect the running of your program will work something like this;
     ### this is not mandatory and you could have something else below entirely.
-    # nn = NeuralNetwork([3, 6, 3])
+    nn = NeuralNetwork([6, 6])
     # nn.back_propagation_learning(training)
 
 if __name__ == "__main__":
